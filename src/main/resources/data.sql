dc525b4e24f452c7f238fff165bd937802fabd0c
JinHeeDo-prog <gmleh03161@gmail.com>
2023-10-13 20:17:27 +0900

    Blog Modify, Create Method

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO Article (title, content,created_at,updated_at) VALUES ('제목1', '내용 1',NOW(),NOW());
-INSERT INTO ARTICLE (title, content,created_at,updated_at) VALUES ('제목1', '내용 1',NOW(),NOW());
-INSERT INTO ARTICLE (title, content,created_at,updated_at) VALUES ('제목1', '내용 1',NOW(),NOW());
-INSERT INTO ARTICLE (title, content,created_at,updated_at) VALUES ('제목1', '내용 1',NOW(),NOW());
-INSERT INTO ARTICLE (title, content,created_at,updated_at) VALUES ('제목1', '내용 1',NOW(),NOW());
+INSERT INTO Article (title, content,created_at,updated_at) VALUES ('제목1', '나의 블로그',NOW(),NOW());
+INSERT INTO ARTICLE (title, content,created_at,updated_at) VALUES ('제목2', '에 오신걸',NOW(),NOW());
+INSERT INTO ARTICLE (title, content,created_at,updated_at) VALUES ('제목3', '환영합니다',NOW(),NOW());
+INSERT INTO ARTICLE (title, content,created_at,updated_at) VALUES ('제목4', '마음껏 ',NOW(),NOW());
+INSERT INTO ARTICLE (title, content,created_at,updated_at) VALUES ('제목5', '구경하다 가주세요',NOW(),NOW());
